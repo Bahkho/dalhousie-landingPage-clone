1469b84e0c697aa6d32e0b863ce954a6cff7860e
akorede <akoredebakare4u@gmail.com>
2022-06-22 14:58:34 +0100

    dalhousie

---
+++ script.js
@@ -0,0 +1,39 @@
+const campus = document.querySelector("summary");
+const flexCampus = document.getElementById("campus-flex");
+
+campus.addEventListener("click", () => {
+  if (flexCampus.style.display === "block") {
+    flexCampus.style.display = "none";
+  } else {
+    flexCampus.style.display = "block";
+  }
+});
+
+const more = document.getElementById("more");
+const more_content = document.querySelector(".more-content");
+
+more.addEventListener("click", () => {
+  if (more) {
+    more_content.classList.toggle("active");
+  }
+});
+
+const search = document.getElementById("search");
+const bgSearch = document.querySelector(".query");
+
+search.addEventListener("click", () => {
+  if (bgSearch.style.display === "flex") {
+    bgSearch.style.display = "none";
+  } else {
+    bgSearch.style.display = "flex";
+  }
+});
+
+const menu = document.getElementById("menu");
+const lapse = document.querySelector(".lapse");
+
+menu.addEventListener("click", () => {
+  if (menu) {
+    lapse.classList.toggle("active");
+  }
+});
